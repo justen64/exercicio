c9b2f4081b2630cb34d34d66862522de5661bd2e
Rodrigo Justen dos Santos Francisco <PET0035263@aluno.firjansenaisesi.com.br>
2023-09-20 21:05:19 -0300

    bla

--- exercicio3/testetrabalho.js
+++ exercicio3/testetrabalho.js
@@ -1,64 +1,71 @@
 function testetrabalho() {
 
-alert("Bem vindo ao CineSenai")
+    alert("Bem vindo ao CineSenai")
 
-let escolha = 0
-let listaCadastro = [
-    ['admin', 'adminsenha', 2],
-    ['cadastro', 'cadastrosenha', 3],
-    ['eu','senha', 0]
-]
- while (escolha != 4) {
-    escolha = parseInt(prompt("1-Cadastre-se\n2-Login\n3-Filmes\n-4 Sair"));
-    
-    switch (escolha) {
-        case 1:
-            let cadastro = prompt("Digite o seu Usuário");
-            let senha = prompt ("Digite uma senha");
-            let senhaConfirma = prompt ("Digite a senha novamente")
-            
-            for (let i = 0; i < listaCadastro.length; i++){
-                if (cadastro == listaCadastro[i][0]) {
-                    alert ("Esse usuário já existe");
-                }    
-                
-            }        
-            if (senha == senhaConfirma) {
-                alert = ("Cadastro Concluído.")
-            } else { 
-                alert ("Senhas incorretas")
-            }
+    let escolha = 0
+    let listaCadastro = [
+        ['admin', 'adminsenha', 2],
+        ['cadastro', 'cadastrosenha', 3],
+        ['eu', 'senha', 0]
+    ]
+    while (escolha != 4) {
+        escolha = parseInt(prompt("1-Cadastre-se\n2-Login\n3-Filmes\n-4 Sair"));
 
-            
-            let novoCadastro = [cadastro, senha];
-            listaCadastro.push(novoCadastro);
+        switch (escolha) {
+            case 1:
+                let cadastro = prompt("Digite o seu Usuário");
+                let senha = prompt("Digite uma senha");
+                let senhaConfirma = prompt("Digite a senha novamente")
 
-            break;
-            
-            case 2:
-                let login = prompt ("DIgite seu usuário")
-                let loginSenha = prompt ("Digite sua senha")
-                
-                for(let i = 0; i < listaCadastro.length; i++){
-                    let logado = false;
-                    if (login == listaCadastro[i][0] && loginSenha == listaCadastro[i][1]) {
-                        alert ("Bem vindo ao CineSenai");  
-                        logado =true;                   
-                    } 
+                for (let i = 0; i < listaCadastro.length; i++) {
+                    if (cadastro == listaCadastro[i][0]) {
+                        alert("Esse usuário já existe");
+                    }
+
                 }
-                if (!logado) {
-                alert ("Usuário ou senha incorredas");
+                if (senha == senhaConfirma) {
+                    alert = ("Cadastro Concluído.")
+                } else {
+                    alert("Senhas incorretas")
+                }
+
+
+                let novoCadastro = [cadastro, senha];
+                listaCadastro.push(novoCadastro);
+
+                break;
+
+            case 2:
+                logado = login();
+                if (logado) {
+                    menuFilmes();
                 }
                 break;
-            }
-            
         }
-        
-        
+
     }
 
 
+}
 
-    
 
-testetrabalho ()
+function login() {
+    let login = prompt("DIgite seu usuário")
+    let loginSenha = prompt("Digite sua senha")
+    let logado = false;
+
+    for (let i = 0; i < listaCadastro.length; i++) {
+        if (login == listaCadastro[i][0] && loginSenha == listaCadastro[i][1]) {
+            alert("Bem vindo ao CineSenai");
+            logado = true;
+            return logado;
+        }
+    }
+    if (!logado) {
+        alert("Usuário ou senha incorredas");
+        return logado;
+    }
+}
+
+
+testetrabalho()
---
+++ exercicio3/trabalhoteste2.js
@@ -0,0 +1,180 @@
+function trabalhoteste2() {
+
+    alert("Bem-vindo ao cinesenai");
+
+    let escolha = 0;
+    let listaCadastro = [
+        ['admin', 'adminsenha'],
+        ['cadastro', 'cadastrosenha'],
+        ['eu', 'senha']
+    ];
+    let assentosDisponiveis = [
+        [1],[2],[3],[4],[5],
+        [6],[7],[8],[9],[10],
+        [11],[12],[13],[14],[15],
+        [16],[17],[18],[19],[20],
+        [21],[22],[23],[24],[25]
+    ]
+    let listaFilme;
+    
+    function listaFilmes() {
+        alert("Filmes disponiveis");
+        while (escolha != 4) {
+            escolha = parseInt(prompt("1-Harry Potter\n2-A voz do silencio\n3-ABC"));
+    
+    
+            switch (escolha) {
+                case 1:
+                    assentosSala = [
+                        [true, true, true, true, true],
+                        [true, true, true, true, true],
+                        [true, true, true, true, true],
+                        [true, true, true, true, true],
+                        [true, true, true, true, true],
+                    ]
+                    
+                    function mapa(){
+                        resposta = (`   [===  TELA  ===]\n`)
+                        
+                        for (i=0 ;i<5;i++){
+                            resposta += i + "- "
+                            for (j=0 ;j<5;j++){
+                                let fileira = assentosSala[i]
+                                if(fileira[j] == true) {
+                                    resposta += "[  ] ";
+                                } else {
+                                    resposta += "[X] ";
+                                }
+                                //else
+                                // resposta += "[ X ] ";
+                            }
+                            resposta += "\n";
+                        }
+                        resposta += "     0    1   2   3   4";
+                        let fileira = prompt("Escolha uma fileira\n\n" + resposta);
+                        let coluna = prompt("Escolha uma coluna\n\n" + resposta);
+                    
+                        assentosSala[fileira][coluna] = false;
+                    
+                        mapa();
+                    }
+                    mapa();
+    
+                    break;
+                case 2:
+                    assentosSala = [
+                        [ , , , ,]
+                        [ , , , ,]
+                        [ , , , ,]
+                        [ , , , ,]
+                        [ , , , ,]
+
+
+
+
+
+                    ]
+                            break;
+    
+                        }
+                    }
+                    if (!logado) {
+                        alert("Login ou senha errado");
+                    }
+                    if (logado) {
+                        listaFilmes();
+                    }
+                    break;
+                case 3:
+            }
+        }
+    }
+    function menuLogin(){
+        while (escolha != 4) {
+            escolha = parseInt(prompt("1-Cadastra-se\n2-Login\n3-Sair"));
+    
+    
+            switch (escolha) {
+                case 1:
+                    let cadastro = prompt("Qual seu email?");
+                    let cadastrosenha = prompt("Crie uma senha");
+    
+                    for (let i = 0; i < listaCadastro.length; i++) {
+                        if (cadastro == listaCadastro[i][0]) {
+                            alert("Você já está cadastrado");
+                        }
+                    }
+                    listaCadastro.push([cadastro, cadastrosenha])
+                    alert("Bem-vindo ao cinesenai");
+    
+                    break;
+                case 2:
+                    let login = prompt("Qual seu email?");
+                    let senha = prompt("Crie uma senha");
+                    let logado = false
+                    for (let i = 0; i < listaCadastro.length; i++) {
+                        if (login == listaCadastro[i][0] && senha == listaCadastro[i][1]) {
+                            alert("Você entrou, para bens");
+                            logado = true;
+                            break;
+    
+                        }
+                    }
+                    if (!logado) {
+                        alert("Login ou senha errado");
+                    }
+                    if (logado) {
+                        listaFilmes();
+                    }
+                    break;
+                case 3:
+            }
+    }
+    }
+
+    menuLogin();
+}
+trabalhoteste2();
+
+assentosSala = [
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+    [true, true, true, true, true],
+]
+
+function mapa(){
+    resposta = (`   [===  TELA  ===]\n`)
+    
+    for (i=0 ;i<5;i++){
+        resposta += i + "- "
+        for (j=0 ;j<5;j++){
+            let fileira = assentosSala[i]
+            if(fileira[j] == true) {
+                resposta += "[  ] ";
+            } else {
+                resposta += "[X] ";
+            }
+            //else
+            // resposta += "[ X ] ";
+        }
+        resposta += "\n";
+    }
+    resposta += "     0    1   2   3   4";
+    let fileira = prompt("Escolha uma fileira\n\n" + resposta);
+    let coluna = prompt("Escolha uma coluna\n\n" + resposta);
+
+    assentosSala[fileira][coluna] = false;
+
+    mapa();
+}
+mapa();
+
+//     [====  TELA  ====]
+// 1- [ ] [ ] [ ] [ ] [ ]
+// 2- [ ] [ ] [ ] [ ] [ ]
+// 3- [ ] [ ] [ ] [ ] [ ]
+// 4- [ ] [ ] [ ] [ ] [ ]
+// 5- [ ] [ ] [ ] [ ] [ ]
+//     A   B   C   D   E
